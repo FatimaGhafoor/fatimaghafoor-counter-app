c8165265e6f7b71d3a1e8c3db2706169fec1c56e
FatimaGhafoor <channu82411@gmail.com>
2026-07-31 22:14:50 +0500

    feature: handleIncrement feature logic applies

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,24 @@
+import { useState } from "react";
 
+export default function App() {
+  const [count, setCount] = useState(0);
+
+  const handleIncrement = () => {
+    setCount(count + 1);
+  };
+  return (
+    <>
+      <div className="app-container">
+        <h1>Counter App</h1>
+        <p>A simple React mini project.</p>
+      </div>
+
+      <div className="counter-container">
+        <h2>Count:{count}</h2>
+        <button className="increment" onClick={handleIncrement}>
+          + Increment
+        </button>
+      </div>
+    </>
+  );
+}
